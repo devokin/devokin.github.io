3b162c20513f57a92d0b3ec4e8b606a77914ce48
devokin <adevokin@gmail.com>
2020-02-09 12:32:54 +0200

    update sendgrid, update project structure

---
+++ src/helpers/sgSubscribe.ts
@@ -0,0 +1,38 @@
+import sgClient from '@sendgrid/client';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+sgClient.setApiKey(process.env.SENDGRID_API_KEY || '');
+console.log(process.env.SENDGRID_API_KEY);
+console.log(process.env.SENDGRID_LANDING_SUBSCRIBE_LIST_ID);
+
+
+export const sgSubscribe = async (contact: ISgContact) => {
+
+  console.log(contact);
+  return sgClient.request({
+    method: 'PUT',
+    url: '/v3/marketing/contacts',
+    body: {
+      list_ids: [ process.env.SENDGRID_LANDING_SUBSCRIBE_LIST_ID ],
+      contacts: [
+        {
+          email: contact.email,
+          first_name: contact.firstName,
+          last_name: contact.lastName,
+          custom_fields: {
+            'e1_T': contact.message
+          },
+        },
+      ],
+    },
+  });
+};
+
+export interface ISgContact {
+  email: string;
+  firstName: string;
+  lastName: string
+  message: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { ISgContact, sgSubscribe } from './helpers/sgSubscribe';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/subscribe', async (req, res) => {
+
+    const contact: ISgContact = {
+        email: 'alexstudinskiy@i.ua',
+        firstName: 'Alex',
+        lastName: 'Studinskiy',
+        message: 'Hey my project is awesome'
+    };
+
+    try {
+        let result = await sgSubscribe(contact);
+        return res.status(200).send(result);
+    } catch(err) {
+        return res.status(400).send(err)
+    }
+
+});
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`App listening on port 8080`);
+});
